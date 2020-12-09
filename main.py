4aed5b6bf1a58785f4e758db1fbfbd9e3de34af7
AbouTheGreat <72754592+AbouTheGreat@users.noreply.github.com>
2020-12-08 19:09:31 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+while True:
+    print("light Level: " + input.light_level())
+    #if light level is greater than 10, then set neopixels off
+    if input.light_level() >= 10 :
+        light.set_all(color.rgb(0, 0, 0))
+    #if light level is less than 10, then set all neopixels on    
+    elif input.light_level() < 10:
+        light.set_all(color.rgb(0, 0, 255))
+
 
