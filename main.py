3ba78f79e9cc4c459112f2b1247711054e1ca68c
AbouTheGreat <72754592+AbouTheGreat@users.noreply.github.com>
2020-12-15 11:12:44 -0500

    Update pxt.json, main.ts, main.py

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 while True:
-    #if light level is between 11 and 20 then set lights to yellow
+    #if light level is equal to or less than 5, then set neopixels blue
     print("light Level: " + input.light_level())
-    if 11 <= input.light_level() <= 20:
-           light.set_all(color.rgb(255, 255, 0))
-
-    #if light level is equal to or less than 9, then set neopixels blue
-    elif input.light_level() <= 9  :
+    if input.light_level() <= 5  :
         print("light Level: " + input.light_level())
         light.set_all(color.rgb(0, 0, 255))
 
-    #if light level is equal to or greater than 10 then set lights off    
-    elif input.light_level() >= 10 :
+    #if light level is equal to or greater than 6 then set lights off    
+    elif input.light_level() >= 6 :
            light.clear()
+
+           
+           
+           
+           
+           
+           
+           #if light level is between 11 and 20 then set lights to yellow
+    #print("light Level: " + input.light_level())
+    #if 11 <= input.light_level() <= 20:
+        #light.set_all(color.rgb(255, 255, 0))
--- main.ts
+++ main.ts
@@ -1,14 +1,11 @@
 while (true) {
-    // if light level is between 11 and 20 then set lights to yellow
+    // if light level is equal to or less than 5, then set neopixels blue
     console.log("light Level: " + input.lightLevel())
-    if (11 <= input.lightLevel() && input.lightLevel() <= 20) {
-        light.setAll(color.rgb(255, 255, 0))
-    } else if (input.lightLevel() <= 9) {
-        // if light level is equal to or less than 9, then set neopixels blue
+    if (input.lightLevel() <= 5) {
         console.log("light Level: " + input.lightLevel())
         light.setAll(color.rgb(0, 0, 255))
-    } else if (input.lightLevel() >= 10) {
-        // if light level is equal to or greater than 10 then set lights off    
+    } else if (input.lightLevel() >= 6) {
+        // if light level is equal to or greater than 6 then set lights off    
         light.clear()
     }
     
