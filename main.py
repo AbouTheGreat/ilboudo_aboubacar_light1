1edce41f1f28f8fa760ca53358277ce60a7c714e
AbouTheGreat <72754592+AbouTheGreat@users.noreply.github.com>
2020-12-18 00:46:28 -0500

    Update pxt.json, main.ts, main.py

--- main.py
+++ main.py
@@ -1,18 +1,38 @@
 while True:
-    #if light level is equal to or less than 5, then set neopixels blue
+    A = 5
+    A = 4
+    A = 6
+    A = 3 
+    A = 7
+    A = 8
+    A = 9
+    A = 10
+    A = 14
+    A = 16
+    
+    
+    #if light level is less than 5, then set neopixels blue
     print("light Level: " + input.light_level())
-    if input.light_level() <= 5  :
-        print("light Level: " + input.light_level())
-        light.set_all(color.rgb(0, 0, 255))
+    if input.light_level() < 5:
+         light.set_all(color.rgb(0, 0, 255))
+    
 
-    #if light level is equal to or greater than 6 then set lights off    
+    
     elif input.light_level() >= 6 :
-           light.clear()
+         light.clear()
+    
+    else:
+        light.set_all(color.rgb(255,165,0))
 
-           
-           
-           
-           
+  
+
+
+
+
+
+
+    #elif 9 >= input.light_level() > 3:
+        #light.set_all(color.rgb(255,165,0))       
            
            
            #if light level is between 11 and 20 then set lights to yellow
--- main.ts
+++ main.ts
@@ -1,12 +1,23 @@
+let A: number;
 while (true) {
-    // if light level is equal to or less than 5, then set neopixels blue
+    A = 5
+    A = 4
+    A = 6
+    A = 3
+    A = 7
+    A = 8
+    A = 9
+    A = 10
+    A = 14
+    A = 16
+    // if light level is less than 5, then set neopixels blue
     console.log("light Level: " + input.lightLevel())
-    if (input.lightLevel() <= 5) {
-        console.log("light Level: " + input.lightLevel())
+    if (input.lightLevel() < 5) {
         light.setAll(color.rgb(0, 0, 255))
     } else if (input.lightLevel() >= 6) {
-        // if light level is equal to or greater than 6 then set lights off    
         light.clear()
+    } else {
+        light.setAll(color.rgb(255, 165, 0))
     }
     
 }
