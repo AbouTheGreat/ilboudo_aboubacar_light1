357eb4e7fb7ff7515aa88d7eb81c93a055750720
AbouTheGreat <72754592+AbouTheGreat@users.noreply.github.com>
2020-12-22 09:35:14 -0500

    Update pxt.json, main.ts, main.py

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
 while True:
     
-    #if light level is less than 5, then set neopixels blue
+    #if light level is greater than 15, then set neopixels off
     print("light Level: " + input.light_level())
-    if input.light_level() < 5:
-         light.set_all(color.rgb(0, 0, 255))
+    if input.light_level() > 15:
+        light.clear()
     
-
-    #if light level is equal to or greater than 6, then set neopixels off
+    #if light level is equal to or greater than 6, then set neopixels blue
     elif input.light_level() >= 6 :
-         light.clear()
+        light.set_all(color.rgb(0, 0, 255))
     
     else:
         light.set_all(color.rgb(255,165,0))
 
   
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- main.ts
+++ main.ts
@@ -1,11 +1,11 @@
 while (true) {
-    // if light level is less than 5, then set neopixels blue
+    // if light level is greater than 15, then set neopixels off
     console.log("light Level: " + input.lightLevel())
-    if (input.lightLevel() < 5) {
-        light.setAll(color.rgb(0, 0, 255))
-    } else if (input.lightLevel() >= 6) {
-        // if light level is equal to or greater than 6, then set neopixels off
+    if (input.lightLevel() > 15) {
         light.clear()
+    } else if (input.lightLevel() >= 6) {
+        // if light level is equal to or greater than 6, then set neopixels blue
+        light.setAll(color.rgb(0, 0, 255))
     } else {
         light.setAll(color.rgb(255, 165, 0))
     }
